a2fb8d553a3c7ae256c70babc9a143f0ac365901
TheHardew <7374061+TheHardew@users.noreply.github.com>
2023-12-28 16:28:07 +0100

    change ebook name

--- makeEbook.sh
+++ makeEbook.sh
@@ -4,7 +4,8 @@ find chapters/ -type f | xargs -P "$(nproc)" -n 1 ./filterPage.py
 ./adjustToc.py
 sed -i 's#<item#<opf:item#g' ebook/content.opf
 
-rm ebook.epub
+name='Ascending the Heavens as an Evil God.epub'
+rm "$name"
 cd ebook
-zip -0X ../ebook.epub mimetype
-zip -9XrD ../ebook.epub * -x mimetype
+zip -0X ../"$name" mimetype
+zip -9XrD ../"$name" * -x mimetype
